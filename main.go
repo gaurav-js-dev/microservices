06e9e5933cd651fa447409ebbe8b2a2d470e60e3
Gaurav <gsharma.jai@salucro.com>
2022-08-23 23:26:19 +0530

    Added XML Support

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	ZipCode string `json:"zip_code"`
+	Name    string `json:"full_name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zipcode"`
 }
 
 func main() {
 
 	//define routes
+	http.HandleFunc("/greet", greet)
 	http.HandleFunc("/customers", getAllCustomers)
 
 	//starting server
@@ -32,7 +34,11 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{"John", "Arizona", "110075"},
 		{"Rob", "Dallas", "110075"},
 	}
-	w.Header().Add("Content-Type", "application/json")
-
-	json.NewEncoder(w).Encode(customers)
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
