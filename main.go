1e924ed4fcf9cdebc50c538d67b1fdb11ac77fef
Gaurav <gsharma.jai@salucro.com>
2022-08-23 23:14:58 +0530

    Added json encoder

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	ZipCode string `json:"zip_code"`
+}
+
 func main() {
 
 	//define routes
-	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	//starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
@@ -18,4 +25,14 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"John", "Arizona", "110075"},
+		{"Rob", "Dallas", "110075"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
 }
