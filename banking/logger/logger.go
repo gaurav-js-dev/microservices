d70cdf8473437263ef31a9b2564808698f356967
Gaurav <gaurav.haxor@gmail.com>
2022-09-17 10:54:16 +0530

    Added uber zap go for logging

---
+++ banking/logger/logger.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var log *zap.Logger
+
+func init() {
+	var err error
+
+	config := zap.NewProductionConfig()
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.StacktraceKey = ""
+	config.EncoderConfig = encoderConfig
+	log, err = config.Build(zap.AddCallerSkip(1))
+
+	if err != nil {
+		panic(err)
+	}
+
+}
