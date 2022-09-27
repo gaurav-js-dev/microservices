26fe34f4478d1d4058db30aa7d771eba5d0b92f2
Gaurav <gaurav.haxor@gmail.com>
2022-09-27 21:22:20 +0530

    Completed data transfer object code

--- banking/service/customerService.go
+++ banking/service/customerService.go
@@ -2,25 +2,21 @@ package service
 
 import (
 	"github.com/gaurav-js-dev/microservices/banking/domain"
+	"github.com/gaurav-js-dev/microservices/banking/dto"
 	"github.com/gaurav-js-dev/microservices/banking/errs"
 )
 
 type CustomerService interface {
-	GetAllCustomer(string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetAllCustomer(string) ([]dto.CustomerResponse, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-// Get customer by id
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
-}
-
 // Get all customer
-func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer, *errs.AppError) {
+func (s DefaultCustomerService) GetAllCustomer(status string) ([]dto.CustomerResponse, *errs.AppError) {
 	if status == "active" {
 		status = "1"
 	} else if status == "inactive" {
@@ -28,7 +24,25 @@ func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer
 	} else {
 		status = ""
 	}
-	return s.repo.FindAll(status)
+	customers, err := s.repo.FindAll(status)
+	if err != nil {
+		return nil, err
+	}
+	response := make([]dto.CustomerResponse, 0)
+	for _, c := range customers {
+		response = append(response, c.ToDto())
+	}
+	return response, err
+}
+
+// Get customer by id
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	}
+	response := c.ToDto()
+	return &response, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
